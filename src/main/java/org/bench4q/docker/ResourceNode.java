5273e03fdbdcb017a19d803c30b64d464b354adc
coderfengyun <chentienan@yeah.net>
2014-08-29 08:49:07 +0800

    now, you can implement Singleton in the way you want
    now, you can implement Singleton in the way you want

--- src/main/java/org/bench4q/docker/ResourceNode.java
+++ src/main/java/org/bench4q/docker/ResourceNode.java
@@ -19,26 +19,29 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
-class Cpu{
+class Cpu {
 	int usage;
 	int cpuId;
+
 	public int getUsage() {
 		return usage;
 	}
+
 	public void setUsage(int usage) {
 		this.usage = usage;
 	}
+
 	public int getCpuId() {
 		return cpuId;
 	}
+
 	public void setCpuId(int cpuId) {
 		this.cpuId = cpuId;
 	}
 }
 
 public class ResourceNode {
-	private static volatile ResourceNode instance;
+	private static volatile ResourceNode instance = new ResourceNode();
 	private int totalCpu;
 	private int freeCpu;
 	private long totalMemory;
@@ -50,24 +53,39 @@ public class ResourceNode {
 	private static final String PROCFS_CPUINFO = "/proc/cpuinfo";
 	private static final String MEMTOTAL_STRING = "MemTotal";
 	private static final String MEMFREE_STRING = "MemFree";
-	private static final Pattern PROCFS_MEMFILE_FORMAT = Pattern.compile("^([a-zA-Z]*):[ \t]*([0-9]*)[ \t]kB");
-	private static final Pattern PROCFS_CPUFILE_FORMAT = Pattern.compile("processor");
-	
-	public static void main(String[] args){
+	private static final Pattern PROCFS_MEMFILE_FORMAT = Pattern
+			.compile("^([a-zA-Z]*):[ \t]*([0-9]*)[ \t]kB");
+	private static final Pattern PROCFS_CPUFILE_FORMAT = Pattern
+			.compile("processor");
+
+	private ResourceNode() {
+		freeCpu = totalCpu = 0;
+		freeCpu *= getVCpuRatio();
+		readSystemInfo();
+		initBlotter();
+		totalCpu = freeCpu;
+		chenckAndUpdateBlotter();
 	}
-	
-	private void readSystemInfo(){
+
+	public static ResourceNode getInstance() {
+		return instance;
+	}
+
+	public static void main(String[] args) {
+	}
+
+	private void readSystemInfo() {
 		BufferedReader bufferedReader = null;
 		Matcher mat = null;
 		try {
 			bufferedReader = new BufferedReader(new FileReader(PROCFS_MEMINFO));
 			String line = null;
-			while((line = bufferedReader.readLine()) != null){
+			while ((line = bufferedReader.readLine()) != null) {
 				mat = PROCFS_MEMFILE_FORMAT.matcher(line);
-				if(mat.find()){
-					if(mat.group(1).equals(MEMTOTAL_STRING))
+				if (mat.find()) {
+					if (mat.group(1).equals(MEMTOTAL_STRING))
 						totalMemory = Long.parseLong(mat.group(2));
-					else if(mat.group(1).equals(MEMFREE_STRING))
+					else if (mat.group(1).equals(MEMFREE_STRING))
 						freeMemory = Long.parseLong(mat.group(2));
 				}
 			}
@@ -76,14 +94,14 @@ public class ResourceNode {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 			bufferedReader = new BufferedReader(new FileReader(PROCFS_CPUINFO));
-			while((line = bufferedReader.readLine()) != null){
+			while ((line = bufferedReader.readLine()) != null) {
 				mat = PROCFS_CPUFILE_FORMAT.matcher(line);
-				if(mat.find())
+				if (mat.find())
 					freeCpu++;
 			}
-			
+
 			try {
 				bufferedReader.close();
 			} catch (IOException e) {
@@ -91,22 +109,22 @@ public class ResourceNode {
 			}
 		} catch (FileNotFoundException e) {
 			// may be it's not Linux, suppose we get 2GB memory and 2 CPU
-			freeMemory = totalMemory = 2*1024*1024;
+			freeMemory = totalMemory = 2 * 1024 * 1024;
 			freeCpu = totalCpu = 2;
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private void initBlotter(){
-		processorQueue = new PriorityQueue<Cpu>(freeCpu, new Comparator<Cpu>(){
-			public int compare(Cpu c1, Cpu c2){
+
+	private void initBlotter() {
+		processorQueue = new PriorityQueue<Cpu>(freeCpu, new Comparator<Cpu>() {
+			public int compare(Cpu c1, Cpu c2) {
 				return c1.getUsage() - c2.getUsage();
 			}
 		});
 		processorList = new ArrayList<Cpu>();
-		for(int i = 0; i < freeCpu; ++i){
+		for (int i = 0; i < freeCpu; ++i) {
 			Cpu cpu = new Cpu();
 			cpu.setCpuId(i);
 			cpu.setUsage(0);
@@ -114,135 +132,119 @@ public class ResourceNode {
 			processorList.add(cpu);
 		}
 	}
-	
-	private void chenckAndUpdateBlotter(){
+
+	private void chenckAndUpdateBlotter() {
 		TestResourceController testResourceController = new TestResourceController();
-		List<Container> runningContainerList = testResourceController.getContainerList();
+		List<Container> runningContainerList = testResourceController
+				.getContainerList();
 		for (Container container : runningContainerList) {
-			container = testResourceController.inspectContainer(container.getId());
+			container = testResourceController.inspectContainer(container
+					.getId());
 			String[] cpus = container.getConfig().getCpuset().split(",");
-			for(int i = 0; i < cpus.length; ++i){
-				if(cpus[i].equals("")){
+			for (int i = 0; i < cpus.length; ++i) {
+				if (cpus[i].equals("")) {
 					for (Cpu cpu : processorList) {
 						cpu.setUsage(cpu.getUsage() + 1);
 					}
 					freeCpu -= processorList.size();
-				}
-				else{
+				} else {
 					Cpu cpu = processorList.get(Integer.valueOf(cpus[i]));
 					cpu.setUsage(cpu.getUsage() + 1);
 					freeCpu--;
 				}
 			}
-			//update freeMem
-			freeMemory -= container.getConfig().getMemory()/1024;
-			//update the Priority Queue
+			// update freeMem
+			freeMemory -= container.getConfig().getMemory() / 1024;
+			// update the Priority Queue
 			int size = processorList.size();
-			for(int i = 0; i < size; ++i){
+			for (int i = 0; i < size; ++i) {
 				Cpu cpu = processorQueue.poll();
 				processorQueue.add(cpu);
 			}
 		}
 	}
-	
-	private int getVCpuRatio(){
+
+	private int getVCpuRatio() {
 		int result = 3;
 		Properties prop = new Properties();
 		try {
-			prop.load(ResourceNode.class.getClassLoader().getResourceAsStream("docker-service.properties"));
+			prop.load(ResourceNode.class.getClassLoader().getResourceAsStream(
+					"docker-service.properties"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		result = Integer.valueOf(prop.getProperty("VCPU_RATIO", "3"));
 		return result;
 	}
-	
-	private ResourceNode(){
-		freeCpu = totalCpu = 0;
-		readSystemInfo();
-		initBlotter();
-		freeCpu *= getVCpuRatio();
-		totalCpu = freeCpu;
-		
-		chenckAndUpdateBlotter();
-	}
-	
+
 	/**
 	 * 
-	 * @param resource request certain resource used to test
+	 * @param resource
+	 *            request certain resource used to test
 	 * @return NOW is CPU setting
 	 */
-	public String requestResource(RequestResource resource){
+	public String requestResource(RequestResource resource) {
 		long testCpu = resource.getCpuNumber();
 		long testMem = resource.getMemoryLimit();
 		String response = null;
 		lock.writeLock().lock();
-		try{
+		try {
 			StringBuilder builder = new StringBuilder();
-			if(freeCpu >= testCpu && freeMemory >= testMem){
-				for(int i = 0; i < testCpu; ++i){
+			if (freeCpu >= testCpu && freeMemory >= testMem) {
+				for (int i = 0; i < testCpu; ++i) {
 					Cpu cpu = processorQueue.poll();
 					cpu.setUsage(cpu.getUsage() + 1);
-					builder.append(cpu.getCpuId()+",");
+					builder.append(cpu.getCpuId() + ",");
 					processorQueue.add(cpu);
 				}
 				builder.deleteCharAt(builder.length() - 1);
 				freeCpu -= testCpu;
 				response = builder.toString();
 			}
-		} finally{
+		} finally {
 			lock.writeLock().unlock();
 		}
-		
+
 		return response;
 	}
-	
-	public void releaseResource(Container container){
+
+	public void releaseResource(Container container) {
 		String[] cpus = container.getConfig().getCpuset().split(",");
 		lock.writeLock().lock();
-		try{
-			//update avalCpu
-			for(int i = 0; i < cpus.length; ++i){
+		try {
+			// update avalCpu
+			for (int i = 0; i < cpus.length; ++i) {
 				Cpu cpu = processorList.get(Integer.valueOf(cpus[i]));
 				cpu.setUsage(cpu.getUsage() - 1);
 				freeCpu++;
 			}
-			//update freeMem
+			// update freeMem
 			freeMemory += container.getConfig().getMemory();
-			//update the Priority Queue
+			// update the Priority Queue
 			int size = processorList.size();
-			for(int i = 0; i < size; ++i){
+			for (int i = 0; i < size; ++i) {
 				Cpu cpu = processorQueue.poll();
 				processorQueue.add(cpu);
 			}
-		} finally{
+		} finally {
 			lock.writeLock().unlock();
 		}
 	}
-	
-	public Resource getCurrentStatus(){
+
+	public Resource getCurrentStatus() {
 		Resource resource = new Resource();
 		lock.readLock().lock();
-		try{
+		try {
 			resource.setFreeCpu(freeCpu);
 			resource.setTotalCpu(totalCpu);
 			resource.setUsedCpu(totalCpu - freeCpu);
 			resource.setTotalMemeory(totalMemory);
 			resource.setFreeMemory(freeMemory);
 			resource.setUsedMemory(totalMemory - freeMemory);
-		} finally{
+		} finally {
 			lock.readLock().unlock();
 		}
 		return resource;
 	}
-	
-	static ResourceNode getInstance(){
-		if(instance == null){
-			synchronized(ResourceNode.class){
-				if(instance == null)
-					instance = new ResourceNode();
-			}
-		}
-		return instance;
-	}
+
 }
---
+++ src/test/java/com/chensha/testapp/docker_api/Test_ResourceNode.java
@@ -0,0 +1,12 @@
+package com.chensha.testapp.docker_api;
+
+import org.bench4q.docker.ResourceNode;
+import org.junit.Test;
+
+public class Test_ResourceNode {
+	@Test
+	public void test() {
+		ResourceNode.getInstance();
+	}
+
+}
